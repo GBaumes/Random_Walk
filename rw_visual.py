4e859dd01d91f165b361e42487b87c1f1f3ad701
GBaumes <baumesgreg@gmail.com>
2023-04-16 17:42:16 -0400

    Refactored fill_walk()

--- rw_visual.py
+++ rw_visual.py
@@ -9,11 +9,10 @@ while True:
 
   # Plot the points in the walk.
   plt.style.use("classic")
-  fig, ax = plt.subplots()
+  fig, ax = plt.subplots(figsize=(15,9))
   point_numbers = range(rw.num_points)
   ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-             edgecolors='none', s=15)
-  
+             edgecolors='none', s=10)
   # Emphasize the first and last points.
   ax.scatter(0,0,c='green',edgecolors='none',s=100)
   ax.scatter(rw.x_values[-1], rw.y_values[-1],c='red',edgecolors='none',s=100)
