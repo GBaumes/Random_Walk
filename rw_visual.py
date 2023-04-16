f862ee0479a5feef9095a2796c941ca5a9ad37c1
GBaumes <baumesgreg@gmail.com>
2023-04-16 02:25:23 -0400

    Remove the axis

--- rw_visual.py
+++ rw_visual.py
@@ -10,10 +10,19 @@ while True:
   # Plot the points in the walk.
   plt.style.use("classic")
   fig, ax = plt.subplots()
-  ax.scatter(rw.x_values, rw.y_values, s=15)
+  point_numbers = range(rw.num_points)
+  ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+             edgecolors='none', s=15)
+  
+  # Emphasize the first and last points.
+  ax.scatter(0,0,c='green',edgecolors='none',s=100)
+  ax.scatter(rw.x_values[-1], rw.y_values[-1],c='red',edgecolors='none',s=100)
   plt.show()
 
-  keep_running = input("Make another walk? (y/n): ")
-  keep_running = keep_running.lower()
+  # Remove the axis.
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
+
+  keep_running = input("Make another walk? (y/n): ").lower()
   if keep_running == 'n':
     break
